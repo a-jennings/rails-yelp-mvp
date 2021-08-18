c03de65be7e09b826732f255a5d64ba5da5b0c73
Drew Jennings <andrew_jennings@ymail.com>
2021-08-18 12:34:13 +0100

    complete seed file with 50 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+valid_categories = %w[chinese italian japanese french belgian]
+
+50.times do
+  Restaurant.create!(name: Faker::Restaurant.name,
+                     address: Faker::Address.street_address,
+                     category: valid_categories.sample,
+                     phone_number: Faker::PhoneNumber.phone_number_with_country_code)
+end
+
+puts 'Seed Complete'
+puts "#{Restaurant.count} restaurants added!"
