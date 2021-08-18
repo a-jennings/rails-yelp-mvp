5c514c98ff296604aef69fbda833cff8e56b6a5e
Drew Jennings <andrew_jennings@ymail.com>
2021-08-18 13:02:32 +0100

    add restaurant page with link_to show page for each restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,24 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurants = Restaurant.new
+  end
+
+  def create
+    @restaurants = Restaurant.new(restaurant_params)
+    @restaurants.save
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,8 @@
 class ReviewsController < ApplicationController
+
+  def create
+  end
+
+  def new
+  end
 end
